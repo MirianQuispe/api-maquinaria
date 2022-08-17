8f52ceb21ed91d9a0a449067fff1669760f11a3c
MirianQuispe <quispemiriamaldana@gmail.com>
2022-08-17 20:53:10 -0300

    inicio del proyecto

---
+++ src/main/java/code/maq/springboot/ApiMaquinariaApplication.java
@@ -0,0 +1,13 @@
+package code.maq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMaquinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMaquinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/maq/springboot/ApiMaquinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package code.maq.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMaquinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
